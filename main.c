af38e1884c36e78d7415c46268c0e8c3fc62d14c
Ethan Bowden <c3234804@uon.edu.au>
2019-04-03 06:51:53 +0000

    trying the commit thingo

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
+/*key is an int
+This cipher can be described mathematically using the modulus operator. The “key” is an integer, k,
+between -25 and 25 (0 and 26 imply ”no encryption”) and each letter is allocated a number:
+a = 0, b = 1, c = 2, ..., z = 25.
+Encryption can then be performed on a message letter, m, by defining the encryption function, e(m), as:
+e(x) = (m + k)(mod 26)
+where “mod” means the modulus operator (% symbol in C).
+The decryption of a cipher text letter, c, can be defined by a decryption function, d(c), as follows:
+d(c) = (c − k)(mod 26)*/
+
+
+
+
+
 #include <stdio>
+
+
+
 int main()
 {
+    
+    
+    
   return 0;
 }
