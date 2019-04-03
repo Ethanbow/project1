c133541128b5d5f38aa8eafedc3ad5fc9edac4be
Ethan Bowden <c3234804@uon.edu.au>
2019-04-03 08:15:33 +0000

    Started doing the first function

--- main.c
+++ main.c
@@ -8,7 +8,18 @@ where “mod” means the modulus operator (% symbol in C).
 The decryption of a cipher text letter, c, can be defined by a decryption function, d(c), as follows:
 d(c) = (c − k)(mod 26)*/
 
+/*
+ write all the stuff need to do in functions
+ Functions needed:
+ 1) Encryption with a rotaion cipher given message and roation amount
+ 2) decryption of an encrypted message with a rotation cipher tect and rotaion amount
+ 3) Encryption using a substituion ciper given message and alphabet substition
+ 4) Decrypt an encrypted message using substituion given cipher text and substition
+ 5) Decrypt an encrypted message with a rotation cyper given text ony
+ 6) decrypt an encrypted message with a substituion cipher text only
+ */
 
+//https://www.thecrazyprogrammer.com/2016/11/caesar-cipher-c-c-encryption-decryption.html
 
 
 
@@ -19,8 +30,28 @@ d(c) = (c − k)(mod 26)*/
 int main()
 {
     
-    
-    
-    
+
   return 0;
+}
+
+
+int EnRotateWithMessAndAmount()
+{
+    char message[];     // the array to store the message into
+    char character;     // wherer the letters will be stored once they have gone into the array
+    int i;              // shows the postion in the array
+    int key;            // stores the key that has been input
+    
+    printf("Enter a message to Encrypt: \n");
+    scanf("%c", &message);
+    printf("Please enter your key: \n");
+    scanf("%d",&key);
+    
+    for (i=0; message[i]; ++i)
+    {
+        character = message[i];      //stores the readed letter from message into character
+        
+        if(character >=)
+    }
+    
 }
