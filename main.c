dd4f268521c198e807bf7fc57f67dba322592b09
Ethan Bowden <c3234804@uon.edu.au>
2019-04-26 22:31:42 +0000

    m

--- main.c
+++ main.c
@@ -8,16 +8,6 @@ where “mod” means the modulus operator (% symbol in C).
 The decryption of a cipher text letter, c, can be defined by a decryption function, d(c), as follows:
 d(c) = (c − k)(mod 26)*/
 
-/*
- write all the stuff need to do in functions
- Functions needed:
- 1) Encryption with a rotaion cipher given message and roation amount
- 2) decryption of an encrypted message with a rotation cipher tect and rotaion amount
- 3) Encryption using a substituion ciper given message and alphabet substition
- 4) Decrypt an encrypted message using substituion given cipher text and substition
- 5) Decrypt an encrypted message with a rotation cyper given text ony
- 6) decrypt an encrypted message with a substituion cypher text only
- */
 
 //https://www.thecrazyprogrammer.com/2016/11/caesar-cipher-c-c-encryption-decryption.html
 //http://che1.vk2dds.net:8080/dashboard/#/ide/ethanbow/LectureDemo
@@ -31,14 +21,17 @@ d(c) = (c − k)(mod 26)*/
 int main()
 {
     
-    FILE *text;
+   /* FILE *text;
     text = fopen("data.txt","r");
     if(text == NULL)
-            perror("fopen()");
-    
-    //char message[1000];
-   // int key;
-   char choice;         //Where the users choice will be storeded for menu
+            perror("fopen()");   */
+   
+   char message[500];
+   int key;
+   char choice;     //Where the users choice will be storeded for menu
+   
+   message[500] = "hello";
+   key = 4;
     
     
     //This is the user menu for choosing the way they want there message to be encrypted or decrypted
@@ -54,12 +47,31 @@ int main()
     
     switch(choice)
     {
-        case 'a':
-        case 'b':
-        case 'c':
-        case 'd':
-        case 'e':
-        case 'f':
+        case 'a':{
+            char encrypt = EncryptRotateWithMessAndAmount(message[500], key);
+            printf("\n",encrypt);
+        }
+        break;
+        case 'b':{
+            
+        }
+        break;
+        case 'c':{
+            
+        }
+        break;
+        case 'd':{
+            
+        }
+        break;
+        case 'e':{
+            
+        }
+        break;
+        case 'f':{
+            
+        }
+        break;
         default: printf("Incorrect option choice %c\nPlease enter a, b, c, d, e, or f as your choice\n", choice); // if the selection is incorrect this will tell the user to tell them there options they can choose to use 
     }
 
@@ -77,19 +89,16 @@ int main()
 
 
 // This is the function that encrypts a given message and with a rotation cipher which changes the amount by a given number
-int EncryptRotateWithMessAndAmount()
+int EncryptRotateWithMessAndAmount(char *message,int key)
 {
-    
-	char message[1000];
 	char character;
 	int i;
-	int keyRotate;
 	
 	for(i = 0; message[i] != '\0'; ++i){
 		character = message[i];
 		
 		if(character >= 'a' && character <= 'z'){
-			character = character + keyRotate;
+			character = character + key;
 			
 			if(character > 'z'){
 				character = character - 'z' + 'a' - 1;
@@ -98,7 +107,7 @@ int EncryptRotateWithMessAndAmount()
 			message[i] = character;
 		}
 		else if(character >= 'A' && character <= 'Z'){
-			character = character + keyRotate;
+			character = character + key;
 			
 			if(character > 'Z'){
 				character = character - 'Z' + 'A' - 1;
@@ -113,10 +122,10 @@ int EncryptRotateWithMessAndAmount()
 
 //This function dycrypts a given message with a roatation cipher if the key is also known
 
-int DecryptRotaionWithMessAndAmount()
+int DecryptRotaionWithMessAndAmount(char *message,int key)
 {
-    char message[1000], character;
-	int i, Rotatekey;
+    char character;
+	int i;
 	
 	
 	for(i = 0; message[i] != '\0'; ++i)
@@ -125,7 +134,7 @@ int DecryptRotaionWithMessAndAmount()
 		
 		if(character >= 'a' && character <= 'z')
 		{
-			character = character - Rotatekey;
+			character = character - key;
 			
 			if(character < 'a')
 				character = character + 'z' - 'a' + 1;
@@ -135,7 +144,7 @@ int DecryptRotaionWithMessAndAmount()
 
 		else if(character >= 'A' && character <= 'Z')
 		{
-			character = character - Rotatekey;
+			character = character - key;
 			
 			if(character < 'A')
 				character = character + 'Z' - 'A' + 1;
@@ -145,9 +154,6 @@ int DecryptRotaionWithMessAndAmount()
 			message[i] = character;
 		}
     }
-
-		//
-		//
 		return message[i];
 }
 	
